b2f7dfca0901aac5c3177154e91d6f6ebb5978b3
Dmitry Nosov <mizai@yandex.ru>
2020-12-18 12:02:02 +0300

    Refactor: error classes

---
+++ scripts/validation.js
@@ -0,0 +1,11 @@
+const formElement = document.querySelector('.popup__form');
+const formInput = formElement.querySelector('.popup__input');
+
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__submit-button',
+//   inactiveButtonClass: 'popup__button_disabled',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__error_visible'
+// }); 
