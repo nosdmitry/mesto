5ce27bf35c15670dcf9c3710caf91c01ca645194
Dmitry Nosov <mizai@yandex.ru>
2020-12-22 23:22:44 +0300

    Refactor: enableValidation starts only once with object as parameter

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,79 +1,88 @@
-function enableValidation(object) {
-  return object;
-}
-
-let config = enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-button',
-  inactiveButtonClass: 'popup__submit-button_type_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible',
-});
-
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, config) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add(config.inputErrorClass);
   errorElement.classList.add(config.errorClass);
   errorElement.textContent = errorMessage;
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, config) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove(config.inputErrorClass);
   errorElement.classList.remove(config.errorClass);
   errorElement.textContent = '';
 };
 
-const isValid = (formElement, inputElement) => {
+const isValid = (formElement, inputElement, config) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, config);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, config);
   }
 };
 
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
+    console.log(!inputElement.validity.valid);
     return !inputElement.validity.valid;
   });
 };
 
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement, config) => {
   if (hasInvalidInput(inputList)) {
+    console.log(buttonElement);
     buttonElement.classList.add(config.inactiveButtonClass);
     buttonElement.disabled = 'disabled';
-  } else {inputList
+  } else {
     buttonElement.classList.remove(config.inactiveButtonClass);
     buttonElement.disabled = false;
   }
 };
 
-const setEventListener = (formElement) => {
+const setEventListener = (formElement, config) => {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
   const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement, config);
   inputList.forEach(inputElement => {
     inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
+      isValid(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
     });
   });
 };
 
-const checkValidity = () => {
+const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
-    setEventListener(formElement);
+    setEventListener(formElement, config);
   });
 };
 
-function hideAllInputsErrors(popupName) {
-  const inputList = Array.from(popupName.querySelectorAll(config.inputSelector));
+function hideAllInputsErrors(config) {
+  const openedPopup = document.querySelector('.popup_opened');
+  const inputList = Array.from(openedPopup.querySelectorAll(config.inputSelector));
   inputList.forEach(inputElement => {
-    hideInputError(popupName, inputElement);
+    hideInputError(openedPopup, inputElement, config);
   });
 }
+
+// function checkButtonState(config) {
+//   const openedPopup = document.querySelector('.popup_opened');
+//   const inputList = Array.from(openedPopup.querySelectorAll(config.inputSelector));
+//   console.log(inputList);
+//   const submitButton = openedPopup.querySelectorAll(config.submitButtonSelector);
+//   toggleButtonState(inputList, submitButton, config);
+// }
+
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_type_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible',
+};
+
+enableValidation(config);
